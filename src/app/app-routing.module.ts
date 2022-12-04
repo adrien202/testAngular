c56e3adea95eb635bb2f551df79b8b1cc296d4ed
Adrien <adrien.abescat@agenium.com>
2022-12-04 14:47:57 +0100

    first real push

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {ChampionshipComponent} from "./modules/championship/championship.component";
+
+const routes: Routes = [
+  {
+  path: '', component: ChampionshipComponent
+}]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/modules/championship/championship.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ChampionshipComponent} from "./championship.component";
+
+
+
+@NgModule({
+  declarations: [ChampionshipComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChampionshipModule { }
